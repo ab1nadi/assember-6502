ccb434e6c49fbe42b6ae2df924d6c93fd6c865bc
abinadi swapp <abinadiswapp@gmail.com>
2022-12-01 21:17:36 -0700

    working on assembler

--- src/assembler.rs
+++ src/assembler.rs
@@ -1,17 +1,22 @@
 mod instruction;
+mod errors;
 
 use std::collections::HashMap;
 use crate::lexical_analyzer::LexicalAnalyzer;
 use crate::assembler::instruction::Instruction;
+use crate::assembler::errors::AssemblerError;
+use crate::lexical_analyzer::TokenType;
+use crate::lexical_analyzer::Token;
 // holds the assembler main struct 
-pub struct Assembler 
+pub struct Assembler
 {
     lexical_analyzer: LexicalAnalyzer,
     symbol_table: HashMap<String, u32>,
+    current_byte: u32,
 }
 
 
-impl Assembler 
+impl Assembler
 {
     // new 
     // return a new assembler 
@@ -21,9 +26,79 @@ impl Assembler
         {
             lexical_analyzer: LexicalAnalyzer::new(file_name, true).unwrap(),
             symbol_table: HashMap::new(),
+            current_byte: 0,
         }
     }
 
+    // run 
+    // runs the assembler 
+    pub fn run(& mut self ) -> Result<(),AssemblerError>
+    {
 
+        self.first_pass()?;
+
+        Ok(())
+    }
+
+
+    // first_pass
+    // finds all the labels on logical lines 
+    fn first_pass(& mut self) ->Result<(),AssemblerError>
+    {
+        
+        for tokenResult in &mut self.lexical_analyzer
+        {   
+            match tokenResult{
+                Ok(Token) =>
+                {
+                    // there is three things we expect here 
+                    // 1. directive
+                    // 2. a label
+                    // 3. a  instruction opcode
+                    match Token.token_type
+                    {
+                        TokenType::Label =>
+                        {
+
+                        },
+                        TokenType::Instruction =>
+                        {
+
+                        }
+                        _=>
+                        {
+
+                        }
+                    }
+                },
+                Err(err)=>
+                {
+                    return Err(AssemblerError::new(err.details.as_str()));
+                }
+
+
+            }
+        }
+
+
+        Ok(())
+    }
+
+
+    // first_pass_label
+    // adds labels and their byte
+    // values to the symbol table
+    fn first_pass_label(token:Token)
+    {
+
+    }
+
+    // first_pass_instruction
+    // checks that instructions match
+    // a given grammar     
+    fn first_pass_instruction(token:Token)
+    {
+
+    }
 }
 
--- src/assembler/instruction.rs
+++ src/assembler/instruction.rs
@@ -3,22 +3,27 @@ use std::collections::HashMap;
 use crate::lexical_analyzer::TokenType;
 use crate::lexical_analyzer::TokenType::*;
 
+mod grammars{
+    use crate::lexical_analyzer::TokenType;
+    use crate::lexical_analyzer::TokenType::*;
+
+    pub const IMMEDIAT2BYTE:[TokenType; 2] = [Hash, Num2Bytes];
+    pub const ZEROPAGE:     [TokenType; 1] = [Num2Bytes];
+    pub const ZEROPAGEX:    [TokenType; 3] = [Num2Bytes, Comma, RegX];
+    pub const ABSOLUTE:     [TokenType; 1] = [Num4Bytes];
+    pub const ABSOLUTEX:    [TokenType; 3] = [Num4Bytes, Comma, RegX]; 
+    pub const ABSOLUTEY:    [TokenType; 3] = [Num4Bytes, Comma, RegY]; 
+    pub const INDIRECTX:    [TokenType; 5] = [LeftParenth, Num2Bytes, Comma, RegX, RightParenth];  
+    pub const INDIRECTY:    [TokenType; 5] = [LeftParenth, Num2Bytes, RightParenth, Comma, RegY];    
+}
+
 // instruction 
 // holds a sring code i.e. "str", "and", etc.
 // holds a hashmap full of possible grammars that this could be 
 pub struct Instruction 
 {
     string_code: String,
-    opcode_grammer: HashMap<char,Vec<TokenType>>
-}
-
-
-// grammar
-// holds a possible grammar for
-// an instruction, it is a list of tokens that are expected 
-pub struct Grammar<'a>
-{
-    gram: &'a Vec<TokenType>
+    opcode_grammer: Vec<(u8, Vec<TokenType>)>
 }
 
 
@@ -31,11 +36,22 @@ impl Instruction
     // paired with their instruction struct  
     pub fn get_map()
     {
+        let mut map: HashMap<String,Instruction> = HashMap::new();
 
+        // lda 
+        map.insert("lda".to_string(), Instruction{
+            string_code:"lda".to_string(),
+            opcode_grammer: vec![
+                (0xa9,  grammars::IMMEDIAT2BYTE.to_vec()),
+                (0xa5,  grammars::ZEROPAGE.to_vec()),
+                (0xb5,  grammars::ZEROPAGEX.to_vec()),
+                (0xad,  grammars::ABSOLUTE.to_vec()),
+                (0xbd,  grammars::ABSOLUTEX.to_vec()),
+                (0xb9,  grammars::ABSOLUTEY.to_vec()),
+                (0xa1,  grammars::INDIRECTX.to_vec()),
+                (0xb1,  grammars::INDIRECTY.to_vec()),
+            
+            ],
+        });
     }
-}
-
-impl<'a> Grammar<'a>
-{
-        pub const IMMEDIAT: [TokenType; 4] = [HexNum, DecNum, DecNum];
 }
